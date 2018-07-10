0f8f6a5c1699d54aae2854b2d2d9e94626b51300
Paul Maurer <paulrmaurer1@gmail.com>
2018-07-10 15:29:13 -0500

    Django settings files reorganized

---
+++ spreadpool/spreadpool/settings/local.py
@@ -0,0 +1,22 @@
+# settings/local.py
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spreadpool_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
+    }
+}
