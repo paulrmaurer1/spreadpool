f5980f502ed75055ac6ed63bea5cd68070592685
Paul Maurer <paulrmaurer1@gmail.com>
2019-03-05 18:38:03 -0600

    Heroku works! File cleanup

--- spreadpool/spreadpool/settings/base.py
+++ spreadpool/spreadpool/settings/base.py
@@ -14,10 +14,7 @@ import os, datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print ("base_dir : ", BASE_DIR)
-
-# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-# print ("project_root : ", PROJECT_ROOT)
+# print ("base_dir : ", BASE_DIR)
 
 # Application definition
 
--- spreadpool/spreadpool/settings/prod.py
+++ spreadpool/spreadpool/settings/prod.py
@@ -4,8 +4,6 @@
 from .base import *
 
 #SECRET_KEY will pull from environment variable in heroku which is set in config vars
-# from boto.s3.connection import S3Connection
-# SECRET_KEY = S3Connection(os.environ['SECRET_KEY'])
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
@@ -15,7 +13,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['spreadpool.herokuapp.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # STATIC_ROOT declaration
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -36,14 +34,10 @@ import dj_database_url
 DATABASES = { 'default': dj_database_url.config(conn_max_age=600, ssl_require=True) }
 # DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
-
+# Add gunicorn to INSTALLED_APPS
 INSTALLED_APPS += ("gunicorn",)
 
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-# import django_heroku
-# django_heroku.settings(locals())
