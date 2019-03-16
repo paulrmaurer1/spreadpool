ef398d5d3b4df4e8abd21639242eed926a986e4f
Paul Maurer <paulrmaurer1@gmail.com>
2019-03-15 22:19:08 -0500

    logo updates

--- spreadpool/spreadpool/settings/prod.py
+++ spreadpool/spreadpool/settings/prod.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 ALLOWED_HOSTS = ['spreadpool.herokuapp.com', 'www.ncaaspreadpool.com', 'ncaaspreadpool.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # Update database for Heroku, psycopg2 required to read from environment variable
 import psycopg2
@@ -24,17 +24,13 @@ conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 # and convert them to something Django can understand.
 import dj_database_url
 DATABASES = { 'default': dj_database_url.config(conn_max_age=600, ssl_require=True) }
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Add gunicorn to INSTALLED_APPS
 INSTALLED_APPS += ("gunicorn",)
 
-# To leverage Mailgun SMTP email service on Heroku
+# For Mailgun SMTP email service on Heroku
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
 EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
 EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
 EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
-
-# EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
-# EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
