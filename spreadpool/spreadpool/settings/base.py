824611e09ec7b9d477cd85b15945608b5c9d31d8
Paul Maurer <paulrmaurer1@gmail.com>
2020-02-20 15:04:29 -0600

    cleaning up base_email template

--- spreadpool/spreadpool/settings/base.py
+++ spreadpool/spreadpool/settings/base.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'django.contrib.sites.middleware.CurrentSiteMiddleware',
 ]
 
 ROOT_URLCONF = 'spreadpool.urls'
