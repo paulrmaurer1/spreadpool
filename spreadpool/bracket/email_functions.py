c18984f3ea594fe3a1ff89dde5d9a649b7485967
Paul Maurer <paulrmaurer1@gmail.com>
2020-02-21 08:20:47 -0600

    base_email template updates

--- spreadpool/bracket/email_functions.py
+++ spreadpool/bracket/email_functions.py
@@ -57,13 +57,23 @@ def email_team_owners(game, outcome):
 			"""
 			# Construct parts of target 1 email
 			subject1 = 'Congrats! Your team, ' + str(game.team1) + ', advances to the next round!'
-			c1 = {'first_name':target_user1.short_name, 'team_w':game.team1, 'team_l':game.team2}
+			c1 = {
+				'first_name':target_user1.short_name,
+				'player_email':target_user1.email,
+				'team_w':game.team1,
+				'team_l':game.team2
+			}
 			msg1_plain = render_to_string(email_dir + 'game_result_a.txt', c1)
 			msg1_html = render_to_string(email_dir + 'game_result_a.html', c1)
 			
 			# Construct parts of target 2 email
 			subject2 = 'Your team, ' + str(game.team2) + ', didn\'t beat the spread :('
-			c2 = {'first_name':target_user2.short_name, 'team_w':game.team1, 'team_l':game.team2}
+			c2 = {
+				'first_name':target_user2.short_name,
+				'player_email':target_user2.email,
+				'team_w':game.team1,
+				'team_l':game.team2
+			}
 			msg2_plain = render_to_string(email_dir + 'game_result_f.txt', c2)
 			msg2_html = render_to_string(email_dir + 'game_result_f.html', c2)
 
