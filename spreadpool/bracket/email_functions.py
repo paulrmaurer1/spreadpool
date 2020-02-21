ebff4c46d08c70e009743321de87b4c09bc2af77
Paul Maurer <paulrmaurer1@gmail.com>
2020-02-21 13:53:57 -0600

    site_url definition

--- spreadpool/bracket/email_functions.py
+++ spreadpool/bracket/email_functions.py
@@ -8,6 +8,7 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
 from django.template import Context
+from django.contrib.sites.models import Site
 
 """
 The below functions execute lookups to populate email messages with to, from, subject, message
@@ -24,6 +25,7 @@ def email_team_owners(game, outcome):
 	matchset = Matchup.objects.filter(game=game.id)
 
 	email_dir = 'bracket/emails/' # directory where all txt & html email templates are located
+	site_url = Site.objects.get_current().domain
 	
 	# For each match that's related to game
 	for match in matchset:
@@ -67,25 +69,26 @@ def email_team_owners(game, outcome):
 				'win_short_name':target_user1.short_name,
 				'lose_first_name':target_user2.first_name,
 				'lose_short_name':target_user2.short_name,
+				'site_url':site_url,
 			}
 
 			# Construct parts of target 1 email
 			c1 = {
 				'target_email':to_target1,
 			}
-			c.update(c1)  # merge context elements specific to target1 email
+			c1.update(c)  # merge context elements specific to target1 email
 			subject1 = 'Congrats! Your team, ' + str(game.team1) + ', advances to the next round!'
-			msg1_plain = render_to_string(email_dir + 'game_result_a.txt', c)
-			msg1_html = render_to_string(email_dir + 'game_result_a.html', c)
+			msg1_plain = render_to_string(email_dir + 'game_result_a.txt', c1)
+			msg1_html = render_to_string(email_dir + 'game_result_a.html', c1)
 			
 			# Construct parts of target 2 email
 			c2 = {
 				'target_email':to_target2,
 			}
-			c.update(c2)  # merge context elements specific to target2 email
+			c2.update(c)  # merge context elements specific to target2 email
 			subject2 = 'Your team, ' + str(game.team2) + ', didn\'t beat the spread :('
-			msg2_plain = render_to_string(email_dir + 'game_result_f.txt', c)
-			msg2_html = render_to_string(email_dir + 'game_result_f.html', c)
+			msg2_plain = render_to_string(email_dir + 'game_result_f.txt', c2)
+			msg2_html = render_to_string(email_dir + 'game_result_f.html', c2)
 
 		
 		# Send email to each target of matchup if gm_updates = True
