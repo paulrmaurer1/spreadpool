af8ee299e9b3a6cb5fc5a5f7a7c6f5c2113f9af0
Paul Maurer <paulrmaurer1@gmail.com>
2022-08-15 15:42:06 -0500

    added staging.py settings file

--- spreadpool/spreadpool/settings/staging.py
+++ spreadpool/spreadpool/settings/staging.py
@@ -11,9 +11,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 ALLOWED_HOSTS = [
-  'spreadpool.herokuapp.com',
-  'www.ncaaspreadpool.com',
-  'ncaaspreadpool.com',
+  'spreadpool-staging.herokuapp.com', # for staging app & pipeline
 ]
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -32,10 +30,14 @@ DATABASES = { 'default': dj_database_url.config(conn_max_age=600, ssl_require=Tr
 # Add gunicorn to INSTALLED_APPS
 INSTALLED_APPS += ("gunicorn",)
 
+# Email settings: emails are created as filed and put in the below directory
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
 # For Mailgun SMTP email service on Heroku
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
-EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
-EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
-EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
-DEFAULT_FROM_EMAIL = 'NCAASpreadpool Administrator <noreply@ncaaspreadpool.com>'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
+# EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
+# EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
+# EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
+# DEFAULT_FROM_EMAIL = 'NCAASpreadpool Administrator <noreply@ncaaspreadpool.com>'
